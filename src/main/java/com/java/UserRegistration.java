e95b21692702116fe70b6f54c59e08da73358c9f
Ranidhumma <ranidhumma8@gmail.com>
2022-03-17 22:50:24 +0530

    welcome

---
+++ src/main/java/com/java/UserRegistration.java
@@ -0,0 +1,44 @@
+package com.java;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	public String regex;
+
+	public boolean FirstName(String firstName) {
+		regex = "[A-Z]{1}[a-z]{2,}";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(firstName);
+		return m.matches();
+	}
+
+	public boolean LastName(String lastName) {
+		regex = "[a-z]{2,}[A-Z]{1}";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(lastName);
+		return m.matches();
+	}
+
+	public boolean Email(String email) {
+		regex = "^[a-z]+([.][a-z]+)*@bl+[.]co([.]in)*";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(email);
+		return m.matches();
+	}
+
+	public boolean MobileNum(String mobile) {
+		regex = "^[1-9]{2} [1-9]{1}[0-9]{9}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(mobile);
+		return m.matches();
+	}
+
+	public boolean PasswordAtleastOneSpecialChar(String pass) {
+		regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(pass);
+		return m.matches();
+	}
+
+}
---
+++ src/test/java/com/java1/UserRegistrationJunit.java
@@ -0,0 +1,64 @@
+package com.java1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.java.UserRegistration;
+
+public class UserRegistrationJunit {
+	UserRegistration j = new UserRegistration();
+
+	/*
+	 * this Test case is used to check our first name passes given regex pattern
+	 */
+	@Test
+	public void testFirstName() {
+
+		boolean result = j.FirstName("Ranii");
+		// assertTrue(result);
+		assertEquals(true, result);
+	}
+
+	/* this Test case is used to check our last name passes given regex pattern */
+	@Test
+	public void testLastName() {
+
+		boolean result = j.LastName("dhummA");
+		// assertTrue(result);
+		assertEquals(true, result);
+	}
+
+	/* this Test case is used to check our email passes given regex pattern */
+	@Test
+	public void testEmail() {
+
+		boolean result = j.Email("xyz.abc@bl.co.in");
+		// assertTrue(result);
+		assertEquals(true, result);
+	}
+
+	/*
+	 * this Test case is used to check our MobileNumber passes given regex pattern
+	 */
+
+	@Test
+	public void testMobileNum() {
+
+		boolean result = j.MobileNum("91 9922554488");
+		// assertTrue(result);
+		assertEquals(true, result);
+	}
+
+	/*
+	 * this Test case is used to check our minimum 8 character password passes given
+	 * regex pattern /* Test case used to test atleast one Special char
+	 */
+	@Test
+	public void testPassword() {
+
+		boolean result = j.PasswordAtleastOneSpecialChar("xyx@123A");
+		// assertTrue(result);
+		assertEquals(true, result);
+	}
+}
