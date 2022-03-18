f8ce41b5c23097e95298ae01a47662b56bce8a64
Ranidhumma <ranidhumma8@gmail.com>
2022-03-18 11:10:14 +0530

    for invalid input throw custom exception

--- src/main/java/com/java/UserRegistration.java
+++ src/main/java/com/java/UserRegistration.java
@@ -13,11 +13,37 @@ public class UserRegistration {
 		return m.matches();
 	}
 
+	public boolean testFirstName(String firstName) {
+		try {
+			if (!FirstName(firstName)) {
+				throw new InvalidException("Invalid firstName");
+			} else {
+				System.out.println("Valid Input");
+			}
+		} catch (InvalidException e) {
+			System.out.println("exception occured" + e);
+		}
+		return FirstName(firstName);
+	}
+
 	public boolean LastName(String lastName) {
 		regex = "[a-z]{2,}[A-Z]{1}";
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(lastName);
 		return m.matches();
+	}
+
+	public boolean testLastName(String lastName) {
+		try {
+			if (!LastName(lastName)) {
+				throw new InvalidException("Invalid lastName");
+			} else {
+				System.out.println("Valid Input");
+			}
+		} catch (InvalidException e) {
+			System.out.println("exception occured" + e);
+		}
+		return LastName(lastName);
 	}
 
 	public boolean Email(String email) {
@@ -27,11 +53,37 @@ public class UserRegistration {
 		return m.matches();
 	}
 
+	public boolean testEmail(String email) {
+		try {
+			if (!Email(email)) {
+				throw new InvalidException("Invalid emailID");
+			} else {
+				System.out.println("Valid Input");
+			}
+		} catch (InvalidException e) {
+			System.out.println("exception occured" + e);
+		}
+		return Email(email);
+	}
+
 	public boolean MobileNum(String mobile) {
 		regex = "^[1-9]{2} [1-9]{1}[0-9]{9}$";
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(mobile);
 		return m.matches();
+	}
+
+	public boolean testMobileNum(String firstName) {
+		try {
+			if (!FirstName(firstName)) {
+				throw new InvalidException("Invalid MobileNumber");
+			} else {
+				System.out.println("Valid Input");
+			}
+		} catch (InvalidException e) {
+			System.out.println("exception occured" + e);
+		}
+		return FirstName(firstName);
 	}
 
 	public boolean PasswordAtleastOneSpecialChar(String pass) {
@@ -41,4 +93,16 @@ public class UserRegistration {
 		return m.matches();
 	}
 
+	public boolean testPasswordAtleastOneSpecialChar(String pass) {
+		try {
+			if (!PasswordAtleastOneSpecialChar(pass)) {
+				throw new InvalidException("Invalid Password");
+			} else {
+				System.out.println("Valid Input");
+			}
+		} catch (InvalidException e) {
+			System.out.println("exception occured" + e);
+		}
+		return PasswordAtleastOneSpecialChar(pass);
+	}
 }
